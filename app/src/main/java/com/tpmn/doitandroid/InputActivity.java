3dc98b461c2948e7bcd0c2a3e18fdba3c1c5ec73
오준영 <ojun-yeong@ojun-yeongs-MacBook-Pro.local>
2021-01-21 15:58:00 +0900

    Do it chapter7 challger 1

---
+++ app/src/main/java/com/tpmn/doitandroid/InputActivity.java
@@ -0,0 +1,36 @@
+package com.tpmn.doitandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+
+public class InputActivity extends AppCompatActivity {
+
+    View thisView;
+    Button backButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        thisView = LayoutInflater.from(this).inflate(R.layout.activity_input, null);
+        setContentView(thisView);
+        backButton = findViewById(R.id.backButton);
+        backButton.setOnClickListener(v -> { finish();});
+        thisView.startAnimation(MainActivity.animLeft);
+    }
+
+    @Override
+    public void onBackPressed() {
+        finish();
+    }
+
+    @Override
+    public void finish() {
+        thisView.startAnimation(MainActivity.animRight);
+        new Handler(getMainLooper()).postDelayed(super::finish, 500);
+    }
+}
--- app/src/main/java/com/tpmn/doitandroid/MainActivity.java
+++ app/src/main/java/com/tpmn/doitandroid/MainActivity.java
@@ -1,11 +1,10 @@
 package com.tpmn.doitandroid;
 
-import android.os.AsyncTask;
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
-import android.widget.ProgressBar;
-import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,72 +13,26 @@ public class MainActivity extends AppCompatActivity {
 
     public static final String EXTRA_MSG = "EXTRA_MSG";
 
-    BackgroundTask backgroundTask;
-    ProgressBar progressBar;
-    Button confirmButton;
-    Button cancelButton;
+    Button nextButton;
+    public static Animation animLeft;
+    public static Animation animRight;
 
-    int value = 0;
+
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        progressBar = findViewById(R.id.progressBar);
-        confirmButton = findViewById(R.id.confirmButton);
-        confirmButton.setOnClickListener(v -> {
-            backgroundTask = new BackgroundTask();
-            backgroundTask.execute();
+        animLeft = AnimationUtils.loadAnimation(this, R.anim.activity_left);
+        animRight = AnimationUtils.loadAnimation(this, R.anim.activity_right);
+
+        nextButton = findViewById(R.id.nextbutton);
+        nextButton.setOnClickListener(v -> {
+            Intent intent = new Intent(this, InputActivity.class);
+//            intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+            startActivity(intent);
         });
-        cancelButton = findViewById(R.id.cancelButton);
-        cancelButton.setOnClickListener(v -> {
-            if(backgroundTask != null) { backgroundTask.cancel(false); }
-        });
-
-    }
-
-    class BackgroundTask extends AsyncTask<Integer, Integer, Integer> {
-
-        @Override
-        protected void onPreExecute() {
-            value = 0;
-            progressBar.setProgress(value);
-        }
-
-        @Override
-        protected Integer doInBackground(Integer... integers) {
-            if(integers.length > 0) {
-                Log.d("speldipn", "doInBackground:" + integers[0]);
-                value = integers[0];
-            }
-            while (!isCancelled()) {
-                value += 1;
-                if (value >= 100) {
-                    break;
-                } else {
-                    publishProgress(value);
-                }
-
-                try { Thread.sleep(100); } catch (Exception ignored) { }
-            }
-
-            return value;
-        }
-
-        @Override
-        protected void onProgressUpdate(Integer... values) {
-            progressBar.setProgress(values[0]);
-        }
-
-        @Override
-        protected void onPostExecute(Integer integer) {
-            progressBar.setProgress(0);
-        }
-
-        @Override
-        protected void onCancelled() {
-            progressBar.setProgress(0);
-        }
     }
 }
