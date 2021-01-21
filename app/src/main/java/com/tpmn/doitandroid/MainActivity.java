c0991809f7cee14923500fd857dfb5f32993bc4e
오준영 <ojun-yeong@ojun-yeong-ui-iMac.local>
2021-01-22 06:59:49 +0900

    Chapter09 challenge 1

--- app/src/main/java/com/tpmn/doitandroid/MainActivity.java
+++ app/src/main/java/com/tpmn/doitandroid/MainActivity.java
@@ -1,92 +1,107 @@
 package com.tpmn.doitandroid;
 
-import android.annotation.SuppressLint;
-import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     public static final String EXTRA_MSG = "EXTRA_MSG";
+    private boolean isRun = false;
+    private int currentIndex = 0;
+    private List<User> userList = new ArrayList<User>();
+    View itemView;
+    TextView nameTextView;
+    TextView phoneTextView;
+    TextView locationTextView;
+    Button startButton;
+    Button stopButton;
 
-    private WebView webView;
-    private Button goButton;
-    private Button showControlButton;
-    private ViewGroup controlView;
-    private EditText urlEditText;
-    private Animation animLeft;
-    private Animation animRight;
+    Animation appearAnim;
+    Animation initAnim;
 
-    @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        userList.add(new User("오준영", "010-7319-5025", "오류동 51-31 포스시티 406호"));
+        userList.add(new User("오자람", "010-0322-7842", "고척동 241-196 홍성파크빌 201호"));
 
-        animLeft = AnimationUtils.loadAnimation(this, R.anim.activity_left);
-        animRight = AnimationUtils.loadAnimation(this, R.anim.activity_right);
-
-        goButton = findViewById(R.id.goButton);
-        showControlButton = findViewById(R.id.showControlButton);
-        controlView = findViewById(R.id.controlView);
-        urlEditText = findViewById(R.id.urlEditText);
-
-        webView = findViewById(R.id.webView);
-        webView.setWebViewClient(new WebViewClient());
-        WebSettings settings = webView.getSettings();
-        settings.setJavaScriptEnabled(true);
-
-        animRight.setAnimationListener(new Animation.AnimationListener() {
+        initAnim = AnimationUtils.loadAnimation(this, R.anim.init);
+        appearAnim = AnimationUtils.loadAnimation(this, R.anim.appear);
+        appearAnim.setAnimationListener(new Animation.AnimationListener() {
             @Override
-            public void onAnimationStart(Animation animation) { }
-
-            @Override
-            public void onAnimationEnd(Animation animation) {
-                controlView.setVisibility(View.GONE);
+            public void onAnimationStart(Animation animation) {
+                currentIndex %= userList.size();
+                setData(userList.get(currentIndex++));
             }
+
+            @Override
+            public void onAnimationEnd(Animation animation) { }
 
             @Override
             public void onAnimationRepeat(Animation animation) { }
         });
-        animLeft.setAnimationListener(new Animation.AnimationListener() {
-            @Override
-            public void onAnimationStart(Animation animation) {
-                controlView.setVisibility(View.VISIBLE);
-            }
 
-            @Override
-            public void onAnimationEnd(Animation animation) {
-            }
+        itemView = findViewById(R.id.itemView);
+        nameTextView = findViewById(R.id.nameTextView);
+        phoneTextView = findViewById(R.id.phoneTextView);
+        locationTextView = findViewById(R.id.locaitonTextView);
 
-            @Override
-            public void onAnimationRepeat(Animation animation) {
-            }
+        startButton = findViewById(R.id.startButton);
+        stopButton = findViewById(R.id.stopButton);
+
+        userList.add(new User("오준영", "010-7319-5025", "오류동 51-31 406호"));
+        userList.add(new User("오자람", "010-5552-0322", "고척동 홍성파크빌 201호"));
+
+        startButton.setOnClickListener(v -> {
+            isRun = true;
+            new Thread(() -> {
+                while (true) {
+                    if (isRun) {
+                        new Handler(getMainLooper()).post(() -> {
+                            itemView.startAnimation(appearAnim);
+                        });
+                        try { Thread.sleep(2000); } catch (Exception e) { }
+                    }
+                }
+            }).start();
         });
 
-        goButton.setOnClickListener(v -> {
-            String url = urlEditText.getText().toString();
-            if(!url.startsWith("http://")) {
-                url = "http://" + url;
-            }
-            webView.loadUrl(url);
-            controlView.startAnimation(animRight);
+        stopButton.setOnClickListener(v -> {
+            isRun = false;
+            itemView.startAnimation(initAnim);
         });
+    }
 
-        showControlButton.setOnClickListener(v -> {
-            controlView.startAnimation(animLeft);
-        });
+    void setData(User user) {
+        nameTextView.setText(user.name);
+        phoneTextView.setText(user.phone);
+        locationTextView.setText(user.location);
+    }
 
+    class User {
+        String name;
+        String phone;
+        String location;
+
+        public User(String name, String phone, String location) {
+            this.name = name;
+            this.phone = phone;
+            this.location = location;
+        }
     }
 }
