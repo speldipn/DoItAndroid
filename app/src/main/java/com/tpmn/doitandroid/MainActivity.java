3b12b7082c69dd70fecafe0e7d2cf5206c43dc02
오준영 <ojun-yeong@ojun-yeongs-MacBook-Pro.local>
2021-01-22 12:27:26 +0900

    Chapter9 challenge 1

--- app/src/main/java/com/tpmn/doitandroid/MainActivity.java
+++ app/src/main/java/com/tpmn/doitandroid/MainActivity.java
@@ -1,9 +1,9 @@
 package com.tpmn.doitandroid;
 
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.Looper;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
@@ -23,55 +23,68 @@ public class MainActivity extends AppCompatActivity {
     private boolean isRun = false;
     private int currentIndex = 0;
     private List<User> userList = new ArrayList<User>();
-    View itemView;
-    TextView nameTextView;
-    TextView phoneTextView;
-    TextView locationTextView;
     Button startButton;
     Button stopButton;
 
+    ViewGroup container;
+
+    View userView1;
+    View userView2;
+
     Animation appearAnim;
+    Animation disappearAnim;
     Animation initAnim;
+
+    private void setUserItem(View view, User user) {
+        TextView nameTextView = view.findViewById(R.id.nameTextView);
+        nameTextView.setText(user.name);
+        TextView phoneTextView = view.findViewById(R.id.phoneTextView);
+        phoneTextView.setText(user.phone);
+        TextView locationTextView = view.findViewById(R.id.locaitonTextView);
+        locationTextView.setText(user.location);
+    }
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        initAnim = AnimationUtils.loadAnimation(this, R.anim.init);
-        appearAnim = AnimationUtils.loadAnimation(this, R.anim.appear);
-        appearAnim.setAnimationListener(new Animation.AnimationListener() {
-            @Override
-            public void onAnimationStart(Animation animation) {
-                currentIndex %= userList.size();
-                setData(userList.get(currentIndex++));
-            }
-
-            @Override
-            public void onAnimationEnd(Animation animation) { }
-
-            @Override
-            public void onAnimationRepeat(Animation animation) { }
-        });
-
-        itemView = findViewById(R.id.itemView);
-        nameTextView = findViewById(R.id.nameTextView);
-        phoneTextView = findViewById(R.id.phoneTextView);
-        locationTextView = findViewById(R.id.locaitonTextView);
-
-        startButton = findViewById(R.id.startButton);
-        stopButton = findViewById(R.id.stopButton);
-
         userList.add(new User("Neo", "010-3333-1111", "오류동 123-456 106호"));
         userList.add(new User("Rami", "010-5555-2222", "고척동 456-123 101호"));
 
+        container = findViewById(R.id.container);
+
+        userView1 = LayoutInflater.from(this).inflate(R.layout.user_item, container, false);
+        setUserItem(userView1, userList.get(0));
+        container.addView(userView1);
+
+        userView2 = LayoutInflater.from(this).inflate(R.layout.user_item, container, false);
+        setUserItem(userView2, userList.get(1));
+        container.addView(userView2);
+
+        initAnim = AnimationUtils.loadAnimation(this, R.anim.init);
+        appearAnim = AnimationUtils.loadAnimation(this, R.anim.appear);
+        disappearAnim = AnimationUtils.loadAnimation(this, R.anim.disappear);
+        startButton = findViewById(R.id.startButton);
+        stopButton = findViewById(R.id.stopButton);
+
         startButton.setOnClickListener(v -> {
-            isRun = true;
             new Thread(() -> {
+
+                isRun = true;
                 while (true) {
                     if (isRun) {
                         new Handler(getMainLooper()).post(() -> {
-                            itemView.startAnimation(appearAnim);
+                            if(currentIndex == 0)  {
+                                userView1.startAnimation(appearAnim);
+                                userView2.startAnimation(disappearAnim);
+                                currentIndex = 1;
+                            } else {
+                                userView1.startAnimation(disappearAnim);
+                                userView2.startAnimation(appearAnim);
+                                currentIndex = 0;
+                            }
+
                         });
                         try { Thread.sleep(2000); } catch (Exception ignored) { }
                     }
@@ -81,14 +94,7 @@ public class MainActivity extends AppCompatActivity {
 
         stopButton.setOnClickListener(v -> {
             isRun = false;
-            itemView.startAnimation(initAnim);
         });
-    }
-
-    void setData(User user) {
-        nameTextView.setText(user.name);
-        phoneTextView.setText(user.phone);
-        locationTextView.setText(user.location);
     }
 
     class User {
