207d0a273ced86733809a43d12af0111ba7fb729
오준영 <ojun-yeong@ojun-yeongs-MacBook-Pro.local>
2021-01-25 11:46:22 +0900

    HTTP get samples

--- app/src/main/java/com/tpmn/doitandroid/MainActivity.java
+++ app/src/main/java/com/tpmn/doitandroid/MainActivity.java
@@ -3,6 +3,9 @@ package com.tpmn.doitandroid;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.util.Log;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -10,19 +13,22 @@ import android.widget.TextView;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.DataInputStream;
+import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.net.URLConnection;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String EXTRA_MSG = "EXTRA_MSG";
 
-    EditText editText;
-    Button sendButton;
-    TextView textView;
+    EditText urlEditText;
+    Button getButton;
+
+    TextView htmlTextView;
+    WebView webView;
+
+    String html = "";
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -32,59 +38,69 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void setup() {
-        editText = findViewById(R.id.editText);
-        textView = findViewById(R.id.textView);
-        sendButton = findViewById(R.id.sendButton);
-        sendButton.setOnClickListener(v -> {
-            String url = editText.getText().toString();
+        urlEditText = findViewById(R.id.urlEditText);
+        getButton = findViewById(R.id.getButton);
+        
+        htmlTextView = findViewById(R.id.htmlTextView);
+        webView = findViewById(R.id.webView);
+        
+        getButton.setOnClickListener(v -> {
+            String urlStr = urlEditText.getText().toString();
             new Thread(() -> {
-                request(url);
+                request(urlStr); 
             }).start();
         });
+
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebChromeClient(new WebChromeClient());
     }
 
-    private void printLogMsg(String msg) {
+    private void printMsg(String msg) {
         new Handler(Looper.getMainLooper()).post(() -> {
-            textView.setText(msg);
+          htmlTextView.setText(msg);
+          webView.loadData(html, "text/html", "utf-8");
+            Log.d("speldipn", html);
         });
     }
 
     private void request(String urlStr) {
         StringBuilder output = new StringBuilder();
+        StringBuilder htmlOutput = new StringBuilder();
+
         try {
             if(!urlStr.startsWith("http://")) {
                 urlStr = "http://" + urlStr;
             }
             URL url = new URL(urlStr);
-
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             if(conn != null) {
-                printLogMsg("Connection.");
+                printMsg("Connected.");
                 conn.setConnectTimeout(10000);
                 conn.setRequestMethod("GET");
                 conn.setDoInput(true);
 
-                int resCode = conn.getResponseCode();
-                BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                InputStream is = conn.getInputStream();
+                DataInputStream dis = new DataInputStream(is);
+
                 String line;
-                while(true) {
-                    line = reader.readLine();
-                    if(line == null) {
-                        break;
+                do {
+                    line = dis.readLine();
+                    if(line != null) {
+                        output.append(line + "\n");
+                        htmlOutput.append(line);
                     }
+                } while(line != null);
 
-                    output.append(line + "\n");
-                }
-                reader.close();
+                dis.close();
                 conn.disconnect();
-
             } else {
-                printLogMsg("Connection fail");
+                printMsg("Disconnected.");
             }
-        }catch(Exception e) {
+        } catch(Exception e) {
             e.printStackTrace();
         }
 
-        printLogMsg(output.toString());
+        printMsg(output.toString());
+        html = htmlOutput.toString();
     }
 }
