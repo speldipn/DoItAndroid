83dbd355ebacdc4abe3b68c36da146b2515c0219
오준영 <ojun-yeong@ojun-yeong-ui-iMac.local>
2021-01-26 07:29:06 +0900

    Save or read book using database challene - WIP

--- app/src/main/java/com/tpmn/doitandroid/MainActivity.java
+++ app/src/main/java/com/tpmn/doitandroid/MainActivity.java
@@ -1,127 +1,91 @@
 package com.tpmn.doitandroid;
 
-import android.Manifest;
-import android.content.ContentResolver;
-import android.content.Intent;
-import android.database.Cursor;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteTableLockedException;
 import android.os.Bundle;
-import android.provider.ContactsContract;
-import android.util.Log;
+import android.os.Handler;
+import android.os.Looper;
 import android.widget.Button;
-import android.widget.ImageView;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.io.InputStream;
-
 public class MainActivity extends AppCompatActivity {
 
     public static final String EXTRA_MSG = "EXTRA_MSG";
+    public static final String DB_NAME = "neo.db";
+    public static final String TABLE_NAME = "neo";
+
+    EditText titleEditText;
+    EditText authorEditText;
+    EditText contentEditText;
+    Button saveButton;
 
     TextView textView;
-    Button button;
+
+    SQLiteDatabase database;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setup();
-
-        requestPermissions(new String[]{Manifest.permission.READ_CONTACTS, Manifest.permission.WRITE_CONTACTS}, 1000);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode == 102) {
-            if (resultCode == RESULT_OK) {
-                try {
-                    Uri contactsUri = data.getData();
-                    String id = contactsUri.getLastPathSegment();
-
-                    getContacts(id);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-
-        if (requestCode == 101) {
-            if (resultCode == RESULT_OK) {
-                Uri fileUri = data.getData();
-                Log.d("speldipn", "fileUri: " + fileUri);
-
-                ContentResolver resolver = getContentResolver();
-                try {
-                    InputStream instream = resolver.openInputStream(fileUri);
-                    Bitmap imgBitmap = BitmapFactory.decodeStream(instream);
-//                    imageView.setImageBitmap(imgBitmap);
-
-                    instream.close();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
-
-    private void getContacts(String id) {
-        Cursor cursor = null;
-        String name = "";
-
-        try {
-            cursor = getContentResolver().query(ContactsContract.Data.CONTENT_URI,
-                    null, ContactsContract.Data.CONTACT_ID + "=?", new String[]{id}, null);
-
-            if (cursor.moveToFirst()) {
-                name = cursor.getString(cursor.getColumnIndex(ContactsContract.Data.DISPLAY_NAME));
-                printMsg("Name: " + name);
-
-                String columns[] = cursor.getColumnNames();
-                for (String column : columns) {
-                    int index = cursor.getColumnIndex(column);
-                    String columnOutput = ("#" + index + " -> [" + column + "] " + cursor.getString(index));
-                    printMsg(columnOutput);
-                }
-                cursor.close();
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void printMsg(String msg) {
-        textView.append(msg + "\n");
     }
 
     private void setup() {
-//        imageView = findViewById(R.id.imageView);
-        textView = findViewById(R.id.textView);
-        button = findViewById(R.id.button);
-        button.setOnClickListener(v -> {
-//            openGallery();
-            chooseContacts();
+        titleEditText = findViewById(R.id.titleEditText);
+        authorEditText = findViewById(R.id.authorEditText);
+        contentEditText = findViewById(R.id.contentEditText);
+        saveButton = findViewById(R.id.saveButton);
+        saveButton.setOnClickListener(v -> {
+            saveData();
+        });
+        textView = findViewById(R.id.debugTextView);
+
+        createDdatabase();
+    }
+
+    private void saveData() {
+        String title = titleEditText.getText().toString();
+        String author = authorEditText.getText().toString();
+        String content = contentEditText.getText().toString();
+
+        if (database == null) {
+            printMsg("Database is not exist");
+            return;
+        }
+
+        String sql = "insert (title, author, content) from neo values (" +
+                title + ", " +
+                author + ", " +
+                content +
+                ")";
+
+        database.execSQL(sql);
+    }
+
+    private void getData() {
+        // TODO : read books all
+    }
+
+    private void printMsg(String msg) {
+        new Handler(Looper.getMainLooper()).post(() -> {
+            textView.append(msg + "\n");
         });
     }
 
-    private void chooseContacts() {
-        Intent contactPickerIntent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
-        startActivityForResult(contactPickerIntent, 102);
-    }
+    private void createDdatabase() {
+        String sql = "create table if not exists " + TABLE_NAME + " (" +
+                "title text, " +
+                "author text, " +
+                "content text" +
+                ")";
 
-    private void openGallery() {
-        Intent intent = new Intent();
-        intent.setType("image/*");
-        intent.setAction(Intent.ACTION_GET_CONTENT);
 
-        startActivityForResult(intent, 101);
+        database = SQLiteDatabase.openOrCreateDatabase(DB_NAME, null);
+        database.execSQL(sql);
     }
 }
 
