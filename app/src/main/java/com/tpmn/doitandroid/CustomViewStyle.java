370c33f23877a48bf253390e95da322cba61ca59
오준영 <ojun-yeong@ojun-yeong-ui-iMac.local>
2021-01-27 07:11:03 +0900

    Graphic - WIP

--- app/src/main/java/com/tpmn/doitandroid/CustomViewStyle.java
+++ app/src/main/java/com/tpmn/doitandroid/CustomViewStyle.java
@@ -5,6 +5,7 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.DashPathEffect;
 import android.graphics.Paint;
+import android.graphics.Region;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -35,44 +36,9 @@ public class CustomViewStyle extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
+        canvas.clipRect(220, 240, 250, 270, Region.Op.REPLACE);
         paint.setStyle(Paint.Style.FILL);
         paint.setColor(Color.RED);
-        canvas.drawRect(10, 10, 100, 100, paint);
-
-        paint.setStyle(Paint.Style.STROKE);
-        paint.setStrokeWidth(2.0F);
-        paint.setColor(Color.GREEN);
-        canvas.drawRect(10, 10, 100, 100, paint);
-
-        paint.setStyle(Paint.Style.FILL);
-        paint.setARGB(128, 0, 0, 255);
-        canvas.drawRect(120, 10, 210, 100, paint);
-
-        DashPathEffect dashEffect = new DashPathEffect(new float[]{5, 5}, 1);
-        paint.setStyle(Paint.Style.STROKE);
-        paint.setStrokeWidth(3.0F);
-        paint.setPathEffect(dashEffect);
-        paint.setColor(Color.GREEN);
-        canvas.drawRect(120, 10, 210, 100, paint);
-
-        paint.setStyle(Paint.Style.FILL);
-
-        paint.setColor(Color.MAGENTA);
-        canvas.drawCircle(100, 260, 100, paint);
-
-        paint.setAntiAlias(true);
-        canvas.drawCircle(310, 260, 100, paint);
-
-        paint.setStyle(Paint.Style.STROKE);
-        paint.setStrokeWidth(1);
-        paint.setColor(Color.MAGENTA);
-        paint.setTextSize(30);
-        canvas.drawText("Text (Stroke)", 20, 460, paint);
-
-
-        paint.setStyle(Paint.Style.FILL);
-        paint.setTextSize(30);
-        canvas.drawText("Text", 20, 520, paint);
-
+        canvas.drawRect(220, 240, 320, 340, paint);
     }
 }
