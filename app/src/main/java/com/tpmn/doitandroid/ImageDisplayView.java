f6c73b551b42f4d9233b6898e9a684a0f055b746
오준영 <ojun-yeong@ojun-yeongs-MacBook-Pro.local>
2021-01-27 15:40:23 +0900

    ImageDisplayView last lecture of chapter12 - HOLDING

---
+++ app/src/main/java/com/tpmn/doitandroid/ImageDisplayView.java
@@ -0,0 +1,143 @@
+package com.tpmn.doitandroid;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class ImageDisplayView extends View implements View.OnTouchListener {
+
+    public static String TAG = "ImageDisplayView";
+
+    Context context;
+    Canvas canvas;
+    Bitmap bitmap;
+    Paint paint;
+
+    int lastX;
+    int lastY;
+
+    Bitmap sourceBitmap;
+    Matrix matrix;
+
+    float sourceWidth = 0.0F;
+    float sourceHeight = 0.0F;
+
+    float bitmapCenterX;
+    float bitmapCenterY;
+
+    float scaleRatio;
+    float totalScaleRatio;
+
+    float displayWidth = 0.0F;
+    float displayHeight = 0.0F;
+
+    int displayCenterX;
+    int displayCenterY;
+
+    public float startX;
+    public float startY;
+
+    public static float MAX_SCALE_RATIO = 5.0F;
+    public static float MIN_SCALE_RATIO = 0.1F;
+
+    float oldDistance = 0.0F;
+
+    int oldPointCount = 0;
+    boolean isScrolling = false;
+    float distanceThreshhold = 3.0F;
+
+    public ImageDisplayView(Context context) {
+        super(context);
+        init(context);
+    }
+
+    public ImageDisplayView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+    }
+
+    private void init(Context context) {
+        this.context = context;
+
+        paint = new Paint();
+        matrix = new Matrix();
+
+        setOnTouchListener(this);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        if (w > 0 && h > 0) {
+            newImage(w, h);
+            redraw();
+        }
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        if (bitmap != null) {
+            canvas.drawBitmap(bitmap, 0, 0, null);
+        }
+
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        final int action = event.getAction();
+
+        int pointerCount = event.getPointerCount();
+        Log.d(TAG, "pointerCount: " + pointerCount);
+
+        switch (action) {
+            case MotionEvent.ACTION_DOWN:
+                if(pointerCount == 1) {
+                    float curX = event.getX();
+                    float curY = event.getY();
+
+                    startX = curX;
+                    startY = curY;
+                } else if(pointerCount == 2) {
+                   oldDistance = 0.0F;
+                   isScrolling = true;
+                }
+                return true;
+            case MotionEvent.ACTION_MOVE:
+                if(pointerCount == 1) {
+                    if(isScrolling) {
+                        return true;
+                    }
+
+                    float curX = event.getX();
+                    float curY = event.getY();
+
+                    if (startX == 0.0F) {
+                        startX = curX;
+                        startY = curY;
+
+                        return true;
+                    }
+                } else if(pointerCount == 2) {
+
+                }
+                return true;
+            case MotionEvent.ACTION_UP:
+                return true;
+        }
+
+        return false;
+    }
+
+    private void redraw() {
+    }
+
+    private void newImage(int w, int h) {
+    }
+}
--- app/src/main/java/com/tpmn/doitandroid/MainActivity.java
+++ app/src/main/java/com/tpmn/doitandroid/MainActivity.java
@@ -3,6 +3,7 @@ package com.tpmn.doitandroid;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.view.ViewGroup;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,17 +12,17 @@ public class MainActivity extends AppCompatActivity {
 
     public static final String EXTRA_MSG = "EXTRA_MSG";
 
-    CustomViewDrawingImproved customView;
+    ViewGroup container;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        customView = new CustomViewDrawingImproved(this);
-        setContentView(customView);
+        setContentView(R.layout.activity_main);
         setup();
     }
 
     private void setup() {
+        container = findViewById(R.id.container);
     }
 }
 
