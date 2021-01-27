ace3ca48ad6f82050fff26bb2b33ccb0eee9d282
오준영 <ojun-yeong@ojun-yeongs-MacBook-Pro.local>
2021-01-27 17:06:21 +0900

    init

--- app/src/main/java/com/tpmn/doitandroid/MainActivity.java
+++ app/src/main/java/com/tpmn/doitandroid/MainActivity.java
@@ -18,8 +18,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        CustomViewDrag customView = new CustomViewDrag(this);
-        setContentView(customView);
+        setContentView(R.layout.activity_main);
         setup();
     }
 
