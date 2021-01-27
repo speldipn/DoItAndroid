2a0b3ee5cf0302f1bb38bc9098ea5bb102ec8c01
오준영 <ojun-yeong@ojun-yeongs-MacBook-Pro.local>
2021-01-27 10:19:34 +0900

    CustomViewDrawable

--- app/src/main/java/com/tpmn/doitandroid/CustomViewDrawable.java
+++ app/src/main/java/com/tpmn/doitandroid/CustomViewDrawable.java
@@ -3,8 +3,10 @@ package com.tpmn.doitandroid;
 import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.LinearGradient;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.graphics.Shader;
 import android.graphics.drawable.ShapeDrawable;
 import android.graphics.drawable.shapes.RectShape;
@@ -41,9 +43,9 @@ public class CustomViewDrawable extends View {
         Log.d("speldipn", "width: " + width + ", height: " + height);
 
         Resources res = getResources();
-        int redColor = res.getColor(R.color.color01);
-        int greenColor = res.getColor(R.color.color02);
-        int blueColor = res.getColor(R.color.color03);
+        int blackColor = res.getColor(R.color.color01);
+        int greyColor = res.getColor(R.color.color02);
+        int darkGreyColor = res.getColor(R.color.color03);
 
         upperDrawable = new ShapeDrawable();
 
@@ -54,7 +56,7 @@ public class CustomViewDrawable extends View {
         upperDrawable.setBounds(0, 0, width, height * 2 / 3);
 
         LinearGradient gradient = new LinearGradient(0, 0, 0, height * 2 / 3,
-                greenColor, redColor, Shader.TileMode.CLAMP);
+                greyColor, blackColor, Shader.TileMode.CLAMP);
 
         Paint paint = upperDrawable.getPaint();
 
@@ -69,7 +71,7 @@ public class CustomViewDrawable extends View {
         lowerDrawable.setBounds(0, height * 2 / 3, width, height);
 
         LinearGradient gradient2 = new LinearGradient(0, 0, 0, height * 1 / 3,
-                redColor, blueColor, Shader.TileMode.CLAMP);
+                blackColor, darkGreyColor, Shader.TileMode.CLAMP);
 
         Paint paint2 = lowerDrawable.getPaint();
         paint2.setShader(gradient2);
@@ -79,7 +81,40 @@ public class CustomViewDrawable extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+
         upperDrawable.draw(canvas);
         lowerDrawable.draw(canvas);
+
+        Paint pathPaint = new Paint();
+        pathPaint.setAntiAlias(true);
+        pathPaint.setColor(Color.YELLOW);
+        pathPaint.setStyle(Paint.Style.STROKE);
+        pathPaint.setStrokeWidth(16.0f);
+        pathPaint.setStrokeCap(Paint.Cap.BUTT);
+        pathPaint.setStrokeJoin(Paint.Join.MITER);
+
+        Path path = new Path();
+        path.moveTo(20, 20);
+        path.lineTo(120, 20);
+        path.lineTo(160, 90);
+        path.lineTo(180, 80);
+        path.lineTo(200, 120);
+
+        canvas.drawPath(path, pathPaint);
+
+        pathPaint.setColor(Color.WHITE);
+        pathPaint.setStrokeCap(Paint.Cap.ROUND);
+        pathPaint.setStrokeJoin(Paint.Join.ROUND);
+
+        path.offset(30, 120);
+        canvas.drawPath(path, pathPaint);
+
+        pathPaint.setColor(Color.CYAN);
+        pathPaint.setStrokeCap(Paint.Cap.SQUARE);
+        pathPaint.setStrokeJoin(Paint.Join.BEVEL);
+
+        path.offset(30, 120);
+        canvas.drawPath(path, pathPaint);
+
     }
 }
