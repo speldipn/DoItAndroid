cdb970f8aaa2c373b2e9beb6be08206c17766573
오준영 <ojun-yeong@ojun-yeong-ui-iMac.local>
2021-01-29 23:20:01 +0900

    Clean main

--- app/src/main/java/com/tpmn/doitandroid/MainActivity.java
+++ app/src/main/java/com/tpmn/doitandroid/MainActivity.java
@@ -28,14 +28,6 @@ public class MainActivity extends AppCompatActivity implements AutoPermissionsLi
     public static final String EXTRA_MSG = "EXTRA_MSG";
     public static final int REQ_PERMS = 100;
 
-    MediaPlayer player;
-    MediaRecorder recorder;
-
-    String filename;
-
-    SurfaceHolder holder;
-    FrameLayout previewContainer;
-
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,77 +38,6 @@ public class MainActivity extends AppCompatActivity implements AutoPermissionsLi
     }
 
     private void setup() {
-        SurfaceView surfaceView = new SurfaceView(this);
-        holder = surfaceView.getHolder();
-        holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
-
-        FrameLayout frame = findViewById(R.id.previewContaienr);
-        frame.addView(surfaceView);
-
-        Button startButton = findViewById(R.id.startButton);
-        startButton.setOnClickListener(v -> {
-            startRecording();
-        });
-
-        Button stopButton = findViewById(R.id.stopButton);
-        stopButton.setOnClickListener(v -> {
-            stopRecording();
-        });
-    }
-
-    private void startRecording() {
-        if (recorder == null) {
-            recorder = new MediaRecorder();
-        }
-
-        recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-        recorder.setVideoSource(MediaRecorder.VideoSource.CAMERA);
-        recorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
-        recorder.setAudioEncoder(MediaRecorder.AudioEncoder.DEFAULT);
-        recorder.setVideoEncoder(MediaRecorder.VideoEncoder.DEFAULT);
-        recorder.setOutputFile(filename);
-
-        recorder.setPreviewDisplay(holder.getSurface());
-
-        try {
-            recorder.prepare();
-            recorder.start();
-            Log.d(TAG, "start recording");
-        } catch (Exception e) {
-            e.printStackTrace();
-            recorder.release();
-            recorder = null;
-        }
-    }
-
-    private void stopRecording() {
-        if (recorder == null) {
-            return;
-        }
-
-        recorder.stop();
-        recorder.reset();
-        recorder.release();
-        recorder = null;
-
-        ContentValues values = new ContentValues(10);
-        values.put(MediaStore.MediaColumns.TITLE, "RecordedVideo");
-        values.put(MediaStore.Audio.Media.ALBUM, "Video Album");
-        values.put(MediaStore.Audio.Media.ARTIST, "Mike");
-        values.put(MediaStore.Audio.Media.DISPLAY_NAME, "Recorded Video");
-        values.put(MediaStore.MediaColumns.DATE_ADDED, System.currentTimeMillis() / 1000);
-
-        values.put(MediaStore.MediaColumns.MIME_TYPE, "video/mp4");
-        values.put(MediaStore.Audio.Media.DATA, filename);
-
-        Uri videoUri = getContentResolver().insert(MediaStore.Video.Media.EXTERNAL_CONTENT_URI, values);
-        if (videoUri == null) {
-            Log.d(TAG, "Video insert failed");
-            return;
-        }
-
-        Log.d(TAG, "stop recording");
-        sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, videoUri));
     }
 
     @Override
@@ -132,5 +53,4 @@ public class MainActivity extends AppCompatActivity implements AutoPermissionsLi
     private void showToast(String msg) {
         Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
     }
-
 }
