31dfe060a46164f5255446af5d007f6c0af11f53
오준영 <ojun-yeong@ojun-yeongs-MacBook-Pro.local>
2021-01-29 14:02:01 +0900

    Sample video player

--- app/src/main/java/com/tpmn/doitandroid/MainActivity.java
+++ app/src/main/java/com/tpmn/doitandroid/MainActivity.java
@@ -1,6 +1,10 @@
 package com.tpmn.doitandroid;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +13,11 @@ public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = "speldipn";
     public static final String EXTRA_MSG = "EXTRA_MSG";
+    public static final String ViDEO_URL = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4";
 
+    Button playButton;
+    VideoView videoView;
+    MediaController mediaController;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -20,6 +28,17 @@ public class MainActivity extends AppCompatActivity {
 
 
     private void setup() {
+        playButton = findViewById(R.id.playButton);
+        videoView = findViewById(R.id.videoView);
+
+        mediaController = new MediaController(this);
+        videoView.setMediaController(mediaController);
+
+        playButton.setOnClickListener(v -> {
+            videoView.setVideoURI(Uri.parse(ViDEO_URL));
+            videoView.requestFocus();
+            videoView.start();
+        });
     }
 
 }
