68755cd2dbb232b43d8f2a7b81b7e8e13c7ab568
오준영 <ojun-yeong@ojun-yeong-ui-iMac.local>
2021-01-29 23:17:28 +0900

    Add Camera with mic

--- app/src/main/java/com/tpmn/doitandroid/MainActivity.java
+++ app/src/main/java/com/tpmn/doitandroid/MainActivity.java
@@ -1,17 +1,18 @@
 package com.tpmn.doitandroid;
 
 import android.content.ContentValues;
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.media.MediaRecorder;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.widget.Button;
-import android.widget.MediaController;
+import android.widget.FrameLayout;
 import android.widget.Toast;
-import android.widget.VideoView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,21 +22,19 @@ import com.pedro.library.AutoPermissionsListener;
 
 import org.jetbrains.annotations.NotNull;
 
-import java.io.File;
-
 public class MainActivity extends AppCompatActivity implements AutoPermissionsListener {
 
     public static final String TAG = "speldipn";
     public static final String EXTRA_MSG = "EXTRA_MSG";
-    public static final String ViDEO_URL = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4";
     public static final int REQ_PERMS = 100;
 
-    MediaRecorder recorder;
     MediaPlayer player;
+    MediaRecorder recorder;
 
-    Button startRecordButton;
-    Button stopRecordButton;
     String filename;
+
+    SurfaceHolder holder;
+    FrameLayout previewContainer;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -46,21 +45,23 @@ public class MainActivity extends AppCompatActivity implements AutoPermissionsLi
         AutoPermissions.Companion.loadAllPermissions(this, REQ_PERMS);
     }
 
-
     private void setup() {
-        startRecordButton = findViewById(R.id.startRecordButton);
-        startRecordButton.setOnClickListener(v -> {
+        SurfaceView surfaceView = new SurfaceView(this);
+        holder = surfaceView.getHolder();
+        holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+
+        FrameLayout frame = findViewById(R.id.previewContaienr);
+        frame.addView(surfaceView);
+
+        Button startButton = findViewById(R.id.startButton);
+        startButton.setOnClickListener(v -> {
             startRecording();
         });
-        stopRecordButton = findViewById(R.id.stopRecordButton);
-        stopRecordButton.setOnClickListener(v -> {
+
+        Button stopButton = findViewById(R.id.stopButton);
+        stopButton.setOnClickListener(v -> {
             stopRecording();
         });
-
-        String sdcard = Environment.getExternalStorageDirectory().getAbsolutePath();
-        filename = sdcard + File.separator + "recorded.mp4";
-
-        showToast("sdcard path: " + sdcard);
     }
 
     private void startRecording() {
@@ -69,15 +70,22 @@ public class MainActivity extends AppCompatActivity implements AutoPermissionsLi
         }
 
         recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        recorder.setVideoSource(MediaRecorder.VideoSource.CAMERA);
         recorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
         recorder.setAudioEncoder(MediaRecorder.AudioEncoder.DEFAULT);
+        recorder.setVideoEncoder(MediaRecorder.VideoEncoder.DEFAULT);
         recorder.setOutputFile(filename);
+
+        recorder.setPreviewDisplay(holder.getSurface());
 
         try {
             recorder.prepare();
             recorder.start();
+            Log.d(TAG, "start recording");
         } catch (Exception e) {
             e.printStackTrace();
+            recorder.release();
+            recorder = null;
         }
     }
 
@@ -87,25 +95,28 @@ public class MainActivity extends AppCompatActivity implements AutoPermissionsLi
         }
 
         recorder.stop();
+        recorder.reset();
         recorder.release();
         recorder = null;
 
         ContentValues values = new ContentValues(10);
-        values.put(MediaStore.MediaColumns.TITLE, "Recorded");
-        values.put(MediaStore.Audio.Media.ALBUM, "Audio Album");
-        values.put(MediaStore.Audio.Media.ARTIST, "Audio Album");
-        values.put(MediaStore.Audio.Media.DISPLAY_NAME, "Audio Album");
-        values.put(MediaStore.Audio.Media.IS_RINGTONE, 1);
-        values.put(MediaStore.Audio.Media.IS_MUSIC, 1);
-        values.put(MediaStore.MediaColumns.DATE_ADDED, System.currentTimeMillis()/1000);
-        values.put(MediaStore.MediaColumns.MIME_TYPE, "audio/mp4");
+        values.put(MediaStore.MediaColumns.TITLE, "RecordedVideo");
+        values.put(MediaStore.Audio.Media.ALBUM, "Video Album");
+        values.put(MediaStore.Audio.Media.ARTIST, "Mike");
+        values.put(MediaStore.Audio.Media.DISPLAY_NAME, "Recorded Video");
+        values.put(MediaStore.MediaColumns.DATE_ADDED, System.currentTimeMillis() / 1000);
+
+        values.put(MediaStore.MediaColumns.MIME_TYPE, "video/mp4");
         values.put(MediaStore.Audio.Media.DATA, filename);
 
-        Uri audioUri = getContentResolver().insert(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, values);
-        if(audioUri == null) {
-            showToast("Audio insert failed");
+        Uri videoUri = getContentResolver().insert(MediaStore.Video.Media.EXTERNAL_CONTENT_URI, values);
+        if (videoUri == null) {
+            Log.d(TAG, "Video insert failed");
             return;
         }
+
+        Log.d(TAG, "stop recording");
+        sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, videoUri));
     }
 
     @Override
