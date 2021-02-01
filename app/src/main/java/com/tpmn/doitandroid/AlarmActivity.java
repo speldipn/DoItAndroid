a525aee112bd9acb7c00ad82b6242298f8d54c15
오준영 <ojun-yeong@ojun-yeongs-MacBook-Pro.local>
2021-02-01 17:17:52 +0900

    Vibrate and Ringtone

--- app/src/main/java/com/tpmn/doitandroid/AlarmActivity.java
+++ app/src/main/java/com/tpmn/doitandroid/AlarmActivity.java
@@ -2,13 +2,59 @@ package com.tpmn.doitandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.media.MediaPlayer;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
+import android.view.View;
+import android.widget.Button;
 
 public class AlarmActivity extends AppCompatActivity {
+
+    Button button1;
+    Button button2;
+    Button button3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarm);
+        setup();
     }
+
+    private void setup() {
+        button1 = findViewById(R.id.button1);
+        button1.setOnClickListener(button1Listener);
+
+        button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(button2Listener);
+
+        button3 = findViewById(R.id.button3);
+        button3.setOnClickListener(button3Listener);
+    }
+
+    View.OnClickListener button1Listener = v -> {
+        Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        if(Build.VERSION.SDK_INT > 26) {
+            vibrator.vibrate(VibrationEffect.createOneShot(1000, 10));
+        } else {
+            vibrator.vibrate(1000);
+        }
+    };
+
+    View.OnClickListener button2Listener = v -> {
+        Uri uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        Ringtone ringtone = RingtoneManager.getRingtone(getApplicationContext(), uri);
+        ringtone.play();
+    };
+
+    View.OnClickListener button3Listener = v -> {
+//        MediaPlayer player = MediaPlayer.create(getApplicationContext(), );
+//        player.start();
+    };
 }
