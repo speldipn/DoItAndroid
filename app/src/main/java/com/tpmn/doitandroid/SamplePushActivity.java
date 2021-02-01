508cb202af0f4fa241f2dd25d5df24eae39a69e3
오준영 <ojun-yeong@ojun-yeong-ui-iMac.local>
2021-02-02 05:27:49 +0900

    SamplePush and align android manifest

---
+++ app/src/main/java/com/tpmn/doitandroid/SamplePushActivity.java
@@ -0,0 +1,14 @@
+package com.tpmn.doitandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SamplePushActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sample_push);
+    }
+}
