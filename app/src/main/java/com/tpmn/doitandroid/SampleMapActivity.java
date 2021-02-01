038928038e1dc3c5f3936737dec5db9b01de7509
오준영 <ojun-yeong@ojun-yeongs-MacBook-Pro.local>
2021-02-01 16:52:34 +0900

    Commit WIP

--- app/src/main/java/com/tpmn/doitandroid/SampleMapActivity.java
+++ app/src/main/java/com/tpmn/doitandroid/SampleMapActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -15,11 +16,13 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.gms.common.util.Strings;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.pedro.library.AutoPermissions;
@@ -33,9 +36,9 @@ public class SampleMapActivity extends AppCompatActivity implements AutoPermissi
 
     public static final int REQ_PERMS = 101;
     GoogleMap map;
+    LatLng seoul = new LatLng(37, 127);
     SupportMapFragment mapFragment;
     final String[] permissions = {
-            Manifest.permission.ACCESS_COARSE_LOCATION,
             Manifest.permission.ACCESS_FINE_LOCATION
     };
     Button button;
@@ -55,18 +58,58 @@ public class SampleMapActivity extends AppCompatActivity implements AutoPermissi
         AutoPermissions.Companion.parsePermissions(this, requestCode, permissions, this);
     }
 
+    private boolean checkPerms() {
+        for (int i = 0; i < permissions.length; ++i) {
+            if (checkSelfPermission(permissions[i]) != PackageManager.PERMISSION_GRANTED) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @SuppressLint("MissingPermission")
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (map != null) {
+            if (checkPerms()) {
+                map.setMyLocationEnabled(true);
+            }
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if(map != null) {
+            if(checkPerms()) {
+                map.setMyLocationEnabled(false);
+            }
+        }
+    }
+
+    @SuppressLint("MissingPermission")
     private void setup() {
+        if (!checkPerms()) {
+            return;
+        }
         mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(googleMap -> {
             showToast("Ready maps");
-            if(googleMap != null) {
+            if (googleMap != null) {
                 map = googleMap;
+
+
+                map.moveCamera(CameraUpdateFactory.newLatLng(seoul));
             }
         });
 
         try {
             MapsInitializer.initialize(this);
-        } catch(Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
@@ -75,14 +118,12 @@ public class SampleMapActivity extends AppCompatActivity implements AutoPermissi
             startLocationService();
         });
 
-
     }
 
+    @SuppressLint("MissingPermission")
     private void startLocationService() {
-        for(int i = 0; i < permissions.length; ++i) {
-            if(checkSelfPermission(permissions[i]) != PackageManager.PERMISSION_GRANTED) {
-                return;
-            }
+        if (!checkPerms()) {
+            return;
         }
 
         LocationManager maanger = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
@@ -91,9 +132,13 @@ public class SampleMapActivity extends AppCompatActivity implements AutoPermissi
         Double latitude = location.getLatitude();
         Double longitude = location.getLongitude();
         showToast("Latitude: " + latitude + "\nLongitude: " + longitude);
-        if(map != null) {
-            LatLng seoul = new LatLng(37, 127);
-            map.addMarker(new MarkerOptions().position(seoul).title("Marker in Seoul"));
+        if (map != null) {
+            MarkerOptions markers = new MarkerOptions();
+            markers.title("Marker in Seoul");
+            markers.position(seoul);
+            markers.snippet("Snippet from GPS");
+            markers.icon(BitmapDescriptorFactory.fromResource(R.drawable.mylocation));
+            map.addMarker(markers);
             map.moveCamera(CameraUpdateFactory.newLatLng(seoul));
         }
     }
