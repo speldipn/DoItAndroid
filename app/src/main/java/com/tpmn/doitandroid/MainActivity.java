46d174c59ff82199c9ea9f3ee9363e785193e190
오준영 <ojun-yeong@ojun-yeongs-MacBook-Pro.local>
2021-02-01 17:42:58 +0900

    Notification sample

--- app/src/main/java/com/tpmn/doitandroid/MainActivity.java
+++ app/src/main/java/com/tpmn/doitandroid/MainActivity.java
@@ -1,10 +1,15 @@
 package com.tpmn.doitandroid;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.hardware.Camera;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.SurfaceHolder;
@@ -15,6 +20,7 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
 import com.pedro.library.AutoPermissions;
 import com.pedro.library.AutoPermissionsListener;
@@ -27,6 +33,12 @@ public class MainActivity extends AppCompatActivity implements AutoPermissionsLi
 
     public static final String TAG = "speldipn";
     public static final String EXTRA_MSG = "EXTRA_MSG";
+
+    public static final String CHANNEL_ID = "channel1_id";
+    public static final String CHANNEL_NAME = "channel1_name";
+
+    NotificationManager manager;
+    Button alarmButton;
 
 
     @Override
@@ -46,6 +58,34 @@ public class MainActivity extends AppCompatActivity implements AutoPermissionsLi
     }
 
     private void setup() {
+        alarmButton = findViewById(R.id.alarmButton);
+        alarmButton.setOnClickListener(v -> {
+            showNoti1();
+        });
+    }
+
+    private void showNoti1() {
+        manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        NotificationCompat.Builder builder = null;
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            if(manager.getNotificationChannel(CHANNEL_NAME) == null) {
+                NotificationChannel channel = new NotificationChannel(
+                        CHANNEL_ID,
+                        CHANNEL_NAME,
+                        NotificationManager.IMPORTANCE_DEFAULT
+                );
+                manager.createNotificationChannel(channel);
+                builder = new NotificationCompat.Builder(this, CHANNEL_ID);
+            }
+        } else {
+            builder = new NotificationCompat.Builder(this);
+        }
+        builder.setContentTitle("Simple Alarm");
+        builder.setContentText("Alarm message");
+        builder.setSmallIcon(android.R.drawable.ic_menu_view);
+        Notification noti = builder.build();
+
+        manager.notify(1, noti);
     }
 
 
