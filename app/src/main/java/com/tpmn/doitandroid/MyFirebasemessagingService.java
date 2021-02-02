d80bd2fc6d1321ad209e52f959e4dca5ba64245b
오준영 <ojun-yeong@ojun-yeongs-MacBook-Pro.local>
2021-02-02 10:23:50 +0900

    FCM sample

--- app/src/main/java/com/tpmn/doitandroid/MyFirebasemessagingService.java
+++ app/src/main/java/com/tpmn/doitandroid/MyFirebasemessagingService.java
@@ -3,19 +3,30 @@ package com.tpmn.doitandroid;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.util.Log;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
-public class MyFirebasemessagingService extends Service {
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
     private static final String TAG = "FMS";
 
-    public MyFirebasemessagingService() {
+    public MyFirebaseMessagingService() { }
+
+    @Override
+    public void onNewToken(@NonNull String token) {
+        super.onNewToken(token);
+        Log.d(TAG, "onNewToken called: " + token);
     }
 
-    @Nullable
     @Override
-    public IBinder onBind(Intent intent) {
-        return null;
+    public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+        Log.d(TAG, "onMessageReceived called.");
     }
 }
+
--- app/src/main/java/com/tpmn/doitandroid/SamplePushActivity.java
+++ app/src/main/java/com/tpmn/doitandroid/SamplePushActivity.java
@@ -3,12 +3,59 @@ package com.tpmn.doitandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdReceiver;
+import com.google.firebase.iid.InstanceIdResult;
 
 public class SamplePushActivity extends AppCompatActivity {
+
+    private static final String TAG = "FMS";
+
+    Button button;
+    TextView textView;
+    TextView textView2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sample_push);
+        setup();
+    }
+
+    private void setup() {
+        button = findViewById(R.id.button);
+        textView = findViewById(R.id.textView);
+        textView2 = findViewById(R.id.textView2);
+
+        FirebaseInstanceId.getInstance().getInstanceId().addOnSuccessListener(this, new OnSuccessListener<InstanceIdResult>() {
+            @Override
+            public void onSuccess(InstanceIdResult result) {
+                showToast("onSuccess called.");
+                String newToken = result.getToken();
+                Log.d(TAG, newToken);
+                println(newToken);
+            }
+        });
+
+        button.setOnClickListener(v -> {
+            String instanceId = FirebaseInstanceId.getInstance().getId();
+            String msg = "Valid instane Id: " + instanceId;
+            println(msg);
+            Log.d(TAG, msg);
+        });
+    }
+
+    private void println(String msg){
+        textView2.append(msg + "\n");
+    }
+
+    private void showToast(String msg) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
     }
 }
