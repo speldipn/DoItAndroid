2b7eb8396322bea074827721c87c4b1531eb46d6
오준영 <ojun-yeong@ojun-yeongs-MacBook-Pro.local>
2021-02-26 17:12:57 +0900

    Spinner example

--- app/src/main/java/com/tpmn/doitandroid/PracticeActivity.java
+++ app/src/main/java/com/tpmn/doitandroid/PracticeActivity.java
@@ -5,10 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class PracticeActivity extends AppCompatActivity {
 
@@ -22,24 +29,27 @@ public class PracticeActivity extends AppCompatActivity {
     }
 
     private void setup() {
-        ProgressBar progressBar = findViewById(R.id.progress);
-        TextView textView = findViewById(R.id.textView);
-        Button startButton = findViewById(R.id.startButton);
+        Spinner spinner = findViewById(R.id.spinner);
+        ArrayList<String> data = new ArrayList();
+        data.add("선택하세요");
+        data.add("1월");
+        data.add("2월");
+        data.add("3월");
+        data.add("4월");
+        SpinnerAdapter adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, data);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(), data.get(position), Toast.LENGTH_SHORT).show();
+            }
 
-        startButton.setOnClickListener((v) -> {
-            progressBar.setVisibility(View.VISIBLE);
-            new Thread(() -> {
-                for (int i = 0; i < 3; ++i) {
-                    String msg = String.valueOf(i + 1);
-                    runOnUiThread(() -> textView.setText(msg));
-                    try { Thread.sleep(1000); } catch (Exception ignored) { }
-                }
-                runOnUiThread(() -> {
-                    textView.setText("0");
-                    progressBar.setVisibility(View.INVISIBLE);
-                });
-            }).start();
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
         });
+
     }
 
 }
