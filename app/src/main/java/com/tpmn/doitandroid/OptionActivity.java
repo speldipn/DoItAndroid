20e6c48481f03bc20c229f3b29d39c4b00fd9f11
오준영 <ojun-yeong@ojun-yeongs-MacBook-Pro.local>
2021-04-26 15:46:08 +0900

    options and context menu examples

---
+++ app/src/main/java/com/tpmn/doitandroid/OptionActivity.java
@@ -0,0 +1,87 @@
+package com.tpmn.doitandroid;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.snackbar.Snackbar;
+
+public class OptionActivity extends AppCompatActivity {
+
+    LinearLayout mainView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_option);
+        mainView = findViewById(R.id.mainView);
+        registerForContextMenu(mainView);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.option_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        String msg = "";
+        switch (item.getItemId()) {
+            case R.id.one:
+                msg = getString(R.string.one);
+                break;
+            case R.id.two:
+                msg = getString(R.string.two);
+                break;
+            case R.id.three:
+                msg = getString(R.string.three);
+                break;
+            default:
+        }
+        if (!TextUtils.isEmpty(msg)) {
+            Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        getMenuInflater().inflate(R.menu.context_menu, menu);
+        super.onCreateContextMenu(menu, v, menuInfo);
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        String msg = "";
+        switch (item.getItemId()) {
+            case R.id.first:
+                msg = getString(R.string.first);
+                break;
+            case R.id.second:
+                msg = getString(R.string.second);
+                break;
+            case R.id.third:
+                msg = getString(R.string.third);
+                break;
+            default:
+        }
+
+        if (!TextUtils.isEmpty(msg)) {
+            Snackbar.make(mainView, msg, Snackbar.LENGTH_SHORT).show();
+        }
+
+        return super.onContextItemSelected(item);
+    }
+}
